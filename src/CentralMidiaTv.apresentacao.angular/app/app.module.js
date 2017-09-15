e193c5f61caf17bd486d613acf87e5cbc353ff9b
guilherme rodrigues souza <guilherme-rsouza@hotmail.com>
2017-09-14 23:42:10 -0300

    inserido projeto angular e strutura inicial

---
+++ src/CentralMidiaTv.apresentacao.angular/app/app.module.js
@@ -0,0 +1,18 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('app', [
+        'ngMessages',
+        'ngCookies',
+	    'ui.router',
+	    'ui.bootstrap',
+	    'oc.lazyLoad',
+	    'xenon.controllers',
+	    'xenon.directives',
+	    'xenon.factory',
+	    'xenon.services',
+
+	    // Added in v1.3
+	    'FBAngular'
+    ]);
+})();
---
+++ src/CentralMidiaTv.apresentacao.angular/app/modulo/login/login.controller.js
@@ -0,0 +1,20 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('app').controller('loginCtrl', loginCtrl);
+
+    loginCtrl.$inject = ['$scope', '$rootScope', '$state'];
+    function loginCtrl($scope, $rootScope, $state) {
+        $rootScope.isLoginPage = true;
+        $rootScope.isLightLoginPage = false;
+        $rootScope.isLockscreenPage = false;
+        $rootScope.isMainPage = false;
+
+        //funçoes
+        $scope.login = function() {
+            if (!$scope.formLogin.$valid)
+                return;
+            $state.go('app.dashboard-variant-1');
+        }
+    }
+})();
